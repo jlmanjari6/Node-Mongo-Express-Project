321c9d92251814653efb4df1050ac8fa80dfe621
Lakshmi Manjari Alapati <S530719@nwmissouri.edu>
2018-06-22 13:37:04 +0530

    changed Readme.md with useful comments and instructions.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Node-Mongo-Express-Project
 Student management system
+
+To open/use the application, 
+https://manju-student-management-app.herokuapp.com/ 
+
+MongoDb Atlas cloud connection link,
+mongodb+srv://manju-javvaji_17:manju7@manju-orz4f.mongodb.net/test?retryWrites=true
+
+Heroku deployment method-> Using gitHub. (Alternative was Heroku CLI)
+
--- index.js
+++ index.js
@@ -7,6 +7,9 @@ var upload = multer();
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+
+//Use mongodb://localhost/userdb to connect to the local mongodb, where userdb is the name of the database.
+//Here, we are connecting to the mongo atlas cloud instead of local db.
 mongoose.connect('mongodb+srv://manju-javvaji_17:manju7@manju-orz4f.mongodb.net/test?retryWrites=true')
     .then(() => console.log('connection succesful'))
     .catch((err) => console.error(err));
@@ -32,10 +35,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/Register', registration);
 
-// app.listen(port, () => {
-//     console.log("Server listening on port " + port);
-//   });
-
+//Heroku dynamically assigns our app a port, so we can't set the port to a fixed number. 
+//Heroku adds the port to the env, so we can pull it from there.
+//That way it'll still listen to port 300 when we test locally, but it will also work on Heroku.
 app.listen(process.env.PORT || port , () => {
         console.log("Server listening on port " + port);
       });
